7c32ce815cea588ee0de39a6716f8b2bd0343072
Wu Yuntao <wyt.brandon@gmail.com>
2024-12-24 18:14:01 +0800

    EGC-6590 Update code review prompt (#1)
    * Test prompt
    
    * Debug
    
    * Debug
    
    * Update prompt
    
    * Update

--- src/main.ts
+++ src/main.ts
@@ -85,6 +85,9 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
+- Provide a better translation or correction to improve clarity and readability.
+- For domain models defined in .proto files, ensure that the terminology is consistent throughout the code.
+- To meeting CQRS and Event-Sourcing pattern, verify that event messages end with verbs in past tense and should not end with Event. Ensure that Request and Response messages end with Request and Response respectively. Also confirm that Notice messages end with Notice.
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${
@@ -127,7 +130,7 @@ async function getAIResponse(prompt: string): Promise<Array<{
     const response = await openai.chat.completions.create({
       ...queryConfig,
       // return JSON if the model supports it:
-      ...(OPENAI_API_MODEL === "gpt-4-1106-preview"
+      ...(OPENAI_API_MODEL === "gpt-4-1106-preview" || OPENAI_API_MODEL === "gpt-4o"
         ? { response_format: { type: "json_object" } }
         : {}),
       messages: [
